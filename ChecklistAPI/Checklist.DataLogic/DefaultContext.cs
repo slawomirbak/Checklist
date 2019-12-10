eb811fd86c786c176f27ec2c14e7c6e5b8083caa
Sławomir Bąk <slawek417@gmail.com>
2019-12-10 19:59:23 +0100

    Add default db context

---
+++ ChecklistAPI/Checklist.DataLogic/DefaultContext.cs
@@ -0,0 +1,17 @@
+﻿using Checklist.DataLogic.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checklist.DataLogic
+{
+    public class DefaultContext: IdentityDbContext<IdentityUser>
+    {
+        public DefaultContext(DbContextOptions <DefaultContext> options): base(options)
+        {
+        }
+    }
+}
--- ChecklistAPI/Checklist.Resolver/ServicesRegistrator.cs
+++ ChecklistAPI/Checklist.Resolver/ServicesRegistrator.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Checklist.DataLogic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,6 +13,14 @@ namespace Checklist.Resolver
         public static void RegisterServices(IServiceCollection services)
         {
 
+        }
+
+        public static void AddDatabase(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContextPool<DefaultContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DbDefault"));
+            });
         }
     }
 }
